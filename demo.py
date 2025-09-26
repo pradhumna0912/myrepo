49777a37b902ae32df9424392a0c5f3972bd293a
pradhumna kakad <pradhumnakakad2020@gmail.com>
2025-09-26 14:39:37 +0530

    third v3 of demo.py

--- demo.py
+++ demo.py
@@ -8,6 +8,6 @@ if num > 1:
             print(num, "is not a prime number")
             break
     else:
-        print(num, "this is a prime number")
+        print(num, "this is a Prime number")
 else:
-    print(num, "this is not a prime number")
+    print(num, "this is not a Prime number")
